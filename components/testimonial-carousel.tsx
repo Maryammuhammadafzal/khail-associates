471e046986d485a5621c43518719ad3249de841a
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-12-14 14:27:10 +0500

    testimonial section created

--- components/testimonial-carousel.tsx
+++ components/testimonial-carousel.tsx
@@ -2,31 +2,48 @@ import * as React from "react"
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Quote } from "lucide-react"
+import Image from "next/image"
 
 export function TestimonialCarousel() {
-  return (
-    <Carousel className="w-full max-w-xs">
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
+    return (
+        <Carousel className="w-full h-fit max-w-3xl">
+            <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index}>
+                        <div className="p-1  border-l dark:border-neutral-300/30 border-neutral-400/50">
+                            <Card className="border-none shadow-none ">
+                                <CardContent className="flex flex-col  gap-20 ">
+                                    <Quote size={60} className="text-[#D4AF37] rotate-180" />
+                                    <span className="text-4xl">“I can recommend Archdeco highly enough. They took my ideas and made them even better, and had all sorts of suggestions for details I would never have thought of, which made all the difference to the end result. The team totally got what I wanted and always managed to find just the right piece or the right style.”</span>
+
+                                    <div className="flex justify-between w-full h-auto gap-6">
+                                        <div className=" flex h-auto w-auto gap-4 ">
+                                            <Image src={'/images/testimonials-user-1.webp'} alt="user-image" width={60} height={50} className="rounded-full" />
+                                            <div className="flex flex-col gap-1">
+                                                <h4 className="text-lg uppercase">Bobby Brown</h4>
+                                                <p className="text-neutral-500">Artist</p>
+                                            </div>
+                                        </div>
+
+                                        <div className=" absolute right-10 bottom-6 w-20 h-20 flex gap-3">
+                                            <CarouselPrevious className="" />
+                                            <CarouselNext />
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+
+        </Carousel>
+    )
 }
--- components/testimonial-section.tsx
+++ components/testimonial-section.tsx
@@ -5,10 +5,10 @@ import { TestimonialCarousel } from './testimonial-carousel'
 
 const TestimonialSection = () => {
   return (
-    <section className="philosophy-section bg-[#d4af375b] relative px-8 w-full h-auto min-h-screen flex justify-center items-center">
+    <section className="philosophy-section dark:bg-black relative px-8 w-full h-auto min-h-screen flex justify-center items-center">
 
 
-            <div className="relative w-[40%] h-screen px-12 flex flex-col py-14 justify-between ">
+            <div className="relative w-[35%] h-screen px-12 flex flex-col py-14 justify-center ">
 
                 <div className="w-auto h-screen flex flex-col gap-12">
                     <div data-aos="fade-up" className="sub-heading flex gap-2 items-center">
@@ -24,7 +24,7 @@ const TestimonialSection = () => {
                 </div>
                 <Button data-aos="fade-up" className="bg-neutral-800 text-white w-44 px-10 text-base py-6 rounded-none mt-3 font-semibold flex gap-3 hover:text-neutral-700 hover:bg-white transition delay-150 duration-300 ease-in-out ">Discover More<ArrowUpRight size={16} /></Button>
             </div>
-            <div className='relative w-[60%]  border-l dark:border-neutral-300/30 border-neutral-400/50 h-screen flex justify-center items-center'>
+            <div className='relative w-[65%]  py-24 h-auto flex justify-center items-center'>
                 <TestimonialCarousel />
             </div>
         </section>
