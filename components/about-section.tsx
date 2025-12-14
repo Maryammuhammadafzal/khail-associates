c922bdb756c07a48d5de53d99af138c7f8d9e058
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-12-14 17:32:04 +0500

    footer section creating

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -5,21 +5,21 @@ import Image from 'next/image'
 
 const AboutSection = () => {
     return (
-        <section className="about-section bg-neutral-900 relative w-full h-auto min-h-screen flex flex-col justify-center items-center">
+        <section className="about-section dark:bg-neutral-900 bg-white relative w-full h-auto min-h-screen flex flex-col justify-center items-center">
             <div className="about-bg relative w-full h-screen flex justify-center items-center">
 
                 <div className="w-auto h-screen flex flex-col gap-12 text-center justify-center items-center">
                     <div data-aos="fade-up" className="sub-heading flex gap-2 items-center">
-                        <div className="w-1 h-1 bg-white rounded-full"></div>
-                        <h4 className="text-base letter-spacing text-white uppercase">
+                        <div className="w-1 h-1 dark:bg-white bg-neutral-800 rounded-full"></div>
+                        <h4 className="text-base letter-spacing dark:text-white text-neutral-800 uppercase">
                             Khail Associates Studio
                         </h4>
 
                     </div>
-                    <h1 data-aos="fade-up" className="text-white font-medium text-8xl uppercase letter-spacing max-w-6xl">
+                    <h1 data-aos="fade-up" className="dark:text-white text-neutral-800 font-medium text-8xl uppercase letter-spacing max-w-6xl">
                        We Explore Environments That Integrate Beauty & Function.
                     </h1>
-                    <p data-aos="fade-up" className="text-white text-lg max-w-3xl">We understand every detail that goes into crafting a beautiful space. We use our experience and specialist skills to keep things running smoothly, on budget and on time.</p>
+                    <p data-aos="fade-up" className="dark:text-white text-neutral-800 text-lg max-w-3xl">We understand every detail that goes into crafting a beautiful space. We use our experience and specialist skills to keep things running smoothly, on budget and on time.</p>
                     <Button data-aos="fade-up" className="bg-neutral-800 text-white w-44 px-10 text-base py-6 rounded-none mt-3 font-semibold flex gap-3 hover:text-neutral-700 hover:bg-white transition delay-150 duration-300 ease-in-out ">Discover More <ArrowUpRight size={16} /></Button>
                 </div>
             </div>
