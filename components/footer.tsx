6dfcf654f89911cd5321c1ac875629df1ed45e82
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-12-14 16:26:34 +0500

    footer section creating

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
@@ -26,8 +27,30 @@ const Footer = () => {
             </div>
 
             <div className="footer grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-8">
-                <Image src='/images/image-6.jpeg' alt='Image' className='object-fill w-full h-full' width={200} height={200} />
+                <div className='h-auto w-auto'>
+                    <Image src='/images/Khail-Associates-Logo.jpeg' alt='Image' className='object-fill w-full h-full' width={200} height={200} />
                 </div>
+
+                <div className='w-auto h-auto'>
+                    <ul className="menu-item letter-spacing pr-24 w-auto h-auto gap-2 flex flex-col text-white">
+                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                            <Link href="/services">Services</Link>
+                        </li>
+                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                            <Link href="/projects">Projects</Link>
+                        </li>
+                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                            <Link href="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     )
 }
