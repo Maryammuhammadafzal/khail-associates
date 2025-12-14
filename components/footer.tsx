54b1d5c92b2f39a347a504104b6ae7fac9820728
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-12-14 17:00:54 +0500

    footer section creating

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { FaFacebookF, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <footer className='flex flex-col gap-10 w-full p-6 h-auto justify-center'>
+        <footer className='flex flex-col gap-10 w-full p-6 h-auto justify-center mb-10'>
             <div className="images grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-3">
                 <div className='h-[230px] w-[230px] object-cover '>
                     <Image src='/images/image-1.jpeg' alt='Image' className='object-fill w-full h-full' width={200} height={200} />
@@ -27,28 +28,51 @@ const Footer = () => {
             </div>
 
             <div className="footer grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-8">
-                <div className='h-auto w-auto'>
-                    <Image src='/images/Khail-Associates-Logo.jpeg' alt='Image' className='object-fill w-full h-full' width={200} height={200} />
+                <div className='h-auto w-auto flex flex-col gap-4'>
+                    <Image src='/images/Khail-Associates-Logo.jpeg' alt='Image' className='' width={100} height={100} />
+              <h2 className="dark:text-white text-neutral-800 font-bold text-3xl uppercase">Khail Associates</h2>
+           
                 </div>
 
                 <div className='w-auto h-auto'>
-                    <ul className="menu-item letter-spacing pr-24 w-auto h-auto gap-2 flex flex-col text-white">
-                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                    <ul className="menu-item letter-spacing w-auto h-auto gap-2 flex flex-col ">
+                        <li className="text-base dark:text-white text-neutral-800 hover:text-neutral-500 uppercase">
                             <Link href="/">Home</Link>
                         </li>
-                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                        <li className="text-base dark:text-white text-neutral-800 hover:text-neutral-500 uppercase">
                             <Link href="/about">About</Link>
                         </li>
-                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                        <li className="text-base dark:text-white text-neutral-800 hover:text-neutral-500 uppercase">
                             <Link href="/services">Services</Link>
                         </li>
-                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                        <li className="text-base dark:text-white text-neutral-800 hover:text-neutral-500 uppercase">
                             <Link href="/projects">Projects</Link>
                         </li>
-                        <li data-aos="fade-right" className="text-base text-white hover:text-white/50 uppercase">
+                        <li className="text-base dark:text-white text-neutral-800 hover:text-neutral-500 uppercase">
                             <Link href="/contact">Contact</Link>
                         </li>
                     </ul>
+                </div>
+
+
+                <div className="w-auto h-auto flex flex-col gap-1">
+                    <p className="font-medium text-lg">
+                        +92 332 333333
+                    </p>
+                    <p className="font-medium text-lg">
+                        khailasociates@gmail.com
+                    </p>
+                    <div className="flex gap-4 text-xl">
+
+                        <FaFacebookF />
+                        <FaInstagram />
+                        <FaLinkedin />
+                    </div>
+                </div>
+
+                <div className="w-auto h-auto flex flex-col gap-1">
+                    <p className='text-sm'>Copyright © 2025. All Rights Reserved.</p>
+                    <p className='uppercase text-sm'>Khail Assoaciates</p>
                 </div>
             </div>
         </footer>
--- components/project-section.tsx
+++ components/project-section.tsx
@@ -14,14 +14,14 @@ const ProjectSection = () => {
             <div className="column-1 border-l-[1px] border-white/30 w-[25%] h-screen"></div>
             <div className="column-1 border-l-[1px] border-white/30 w-[25%] h-screen"></div>
           </div>
-            <div className="relative w-full h-screen flex p-8 my-24 flex-col justify-center gap-16">
+            <div className="relative w-full min-h-screen h-auto flex p-8 my-24 flex-col justify-center gap-16">
 
                 <div className="w-auto h-fit flex flex-col gap-12 justify-center ">
                     <h2 data-aos="zoom-in-right" className="text-white font-medium text-9xl uppercase letter-spacing max-w-6xl">
                         Recent Works
                     </h2>
                 </div>
-                <div className='relative w-full h-auto flex justify-center items-center p-3 overflow-hidden'>
+                <div className='relative w-full h-auto flex justify-center items-center p-3 overflow-x-hidden'>
                   <ProjectCarousel />
                 </div>
                 {/* <Button data-aos="fade-up" className="bg-neutral-800 text-white w-44 px-10 text-base py-6 rounded-none mt-3 font-semibold flex gap-3 hover:text-neutral-800 hover:bg-white transition delay-150 duration-300 ease-in-out ">Discover More <ArrowUpRight size={16} /></Button> */}
