8fb023cdaefb8c009d12738ea05a376403fab239
maryammuhammadafzal <natiqnagori7@gmail.com>
2026-02-14 17:00:09 +0500

    created projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,101 @@
-import React from 'react'
+'use client'
 
-const ProjectsPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import { useEffect, useState } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import TestimonialSection from "@/components/testimonial-section";
+import Footer from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+import Service from "@/components/service";
+import ProjectCard from "@/components/project-card";
+
+export default function AnoutPage() {
+    const [theme, setTheme] = useState(true);
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleTheme = () => {
+        const element = document.getElementsByTagName('html')[0];
+        if (theme === true) {
+            setTheme(false);
+            element.classList.add('dark');
+        } else {
+            setTheme(true);
+            element.classList.remove('dark');
+        }
+    }
+
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            duration: 800,
+            // easing: 'ease-in-out',
+        });
+    }, []);
+
+    const projects_data = [
+        {
+            image: '/images/Project-image-1-min.jpeg',
+            title: 'Architect By Yousuf',
+            nickname: 'Architecture'
+        },
+        {
+            image: '/images/Project-image-2-min.jpeg',
+            title: 'Architect By Yousuf',
+            nickname: 'Architecture'
+        },
+        {
+            image: '/images/Project-image-3-min.jpeg',
+            title: 'Architect By Yousuf',
+            nickname: 'Architecture'
+        },
+        {
+            image: '/images/Project-image-4-min.jpeg',
+            title: 'Architect By Yousuf',
+            nickname: 'Architecture'
+        },
+        {
+            image: '/images/Project-image-5-min.jpeg',
+            title: 'Architect By Yousuf',
+            nickname: 'Architecture'
+        },
+        {
+            image: '/images/Project-image-6-min.jpeg',
+            title: 'Architect By Yousuf',
+            nickname: 'Architecture'
+        },
+        {
+            image: '/images/Project-image-7-min.jpeg',
+            title: 'Architect By Yousuf',
+            nickname: 'Architecture'
+        },
+    ]
+
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main className="flex min-h-screen w-full flex-col items-center justify-between bg-white dark:bg-black sm:items-start">
+
+                {/* Hero Section */}
+
+                <HeroSection subheading="Latest Projects" heading="Our Portfolio - Transforming Spaces Across Karachi & Pakistan" paragraph="Explore our diverse portfolio of completed projects across Pakistan. From luxury residences in Karachi's premium localities to commercial developments and industrial facilities, each project showcases our commitment to design excellence and client satisfaction. These are real homes and businesses where families live, children grow, and businesses thrive." image="/images/image-4.jpeg" />
+                {/* <AboutSection /> */}
+
+
+
+                {/* <FeaturesSection /> */}
+                {/* <ProjectSection /> */}
+                <div className="flex flex-wrap px-8 py-24 gap-10">
+
+                    {projects_data.map((feature, index) => (
+                        <ProjectCard key={index} link={'/project/'+feature.title} index={index} title={feature.title} nickname={feature.nickname} image="/images/project-image-1-min.jpeg" />
+                    ))}
+                </div>
+                {/* <Service index={index} icon="/images/feature-icon-1.png" subheading="Our Process" heading="Great Design Starts with Understanding People, Not Just Buildings" paragraph="The main areas of work are architectural design, interior design, project completion, industrial design, ceramic production of decorative items and furniture. Our highly collaborative, award-winning team will help you achieve your goals for a distinctive design, inspired by what you find most important and beautiful." image="/images/project-image-1-min.jpeg" /> */}
+                {/* <AwardsSection /> */}
+                {/* <PhilosophySection /> */}
+                <TestimonialSection />
+                <Footer />
+            </main>
+        </div>
+    );
 }
-
-export default ProjectsPage
---
+++ components/project-card.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Card, CardContent } from './ui/card'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const ProjectCard = ({ link, index, image, title, nickname }: any) => {
+    return (
+        <Link href={link} className="w-auto h-auto">
+            <Card key={index} className="p-3 border-none shadow-none bg-transparent">
+                <CardContent className="flex flex-col gap-6 p-2">
+                    <div className="w-[400px] h-[500px]">
+                        <Image src={image} alt="image" width={400} height={500} className="w-full h-full object-cover object-center" />
+                    </div>
+                    <div className="flex flex-col gap-2 text-neutral-800 dark:text-white">
+                        <h2 className="text-2xl font-medium">{title}</h2>
+                        <p className="font-semibold text-neutral-800/50 dark:text-white/50">{nickname}</p>
+                    </div>
+                </CardContent>
+            </Card>
+        </Link>
+    )
+}
+
+export default ProjectCard
